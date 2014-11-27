df95ae8ac501d0f18b37f101d852e94ac6b70bd2
Erik Bosman <erik@minemu.org>
2014-11-27 22:51:27 +0100

    (manually :-/ ) invert some tiles

--- render_faces.py
+++ render_faces.py
@@ -1,14 +1,16 @@
 
-import globe, math, svg, dhxcut
-from geometry import projection, linear, dhxmath
+import math, sys, os
+
+import globe, svg, dhxcut
+from geometry import projection, linear, dhxmath, dhxlamp
 from render import *
 
 #
 # settings
 #
 
-radius = 100.
-thickness = 2.
+radius = 150.
+thickness = 3.
 overhang = .5
 overcut = 0.
 
@@ -26,42 +28,46 @@ overcut *= native_scale
 
 #
 
+#
+# Sadly, some tiles have to me manually inverted
+#
+inverted = (21, 22, 25, 43, 44)
 
 if __name__ == '__main__':
 
-    engrave = 'stroke:none;fill:#000000;opacity:.3'
-    clip = 'stroke:none;fill:#0000ff;opacity:.3'
-    cut = 'stroke:#ff0000;fill:none;opacity:.3'
+    filename = sys.argv[1]
 
-#    north = projection.ll_to_globe_coord( (133.873987, -23.692496) )
-    m = globe.get_globe_cached()
+    f = open(filename, "w")
+
+    engrave = 'stroke:none;fill:#000000'
+    clip = 'stroke:none;fill:#0000ff'
+    cut = 'stroke:#ff0000;fill:none'
+
     #m = globe.get_globe()
+    m = globe.get_globe_cached()
 
     facepath = svg.polygon_path(dhxcut.shape(radius=radius, thickness=thickness, overhang=overhang, overcut=overcut))
 
     w, h = radius/1.5, radius/1.4
 
-    print svg.header(w*10,h*6)
-    print '<defs><clipPath id="face">'
-    print svg.path( facepath )
-    print '</clipPath></defs>'
+    print >>f,  svg.header(w*10,h*6)
 
-    faces = dhxmath.deltoidalhexecontahedron_faces()
+    faces = dhxlamp.remap_faces(dhxmath.deltoidalhexecontahedron_faces(), dhxlamp.configuration)
 
-    print '<g>'
+    print >>f,  '<g>'
     for i, face in enumerate(faces):
 
-        x, y = i%10, i/10
-        print >>f, '<g transform="translate('+str(w*x+w*.5)+' '+str(h*y+h*.5)+')">'
-        print >>f, svg.path( facepath, cut )
+        x, y = 9-i%10, i/10
+        print >>f,  '<g transform="translate('+str(w*x+w*.5)+' '+str(h*y+h*.4)+')" id="face_'+str(i)+'">'
+        print >>f,  svg.path( facepath, cut )
 #        for slot in dhxcut.slots(radius=radius, native_scale=native_scale):
-#            print >>f, svg.path( svg.polygon_path(slot), cut)
-        print >>f, '</g>'
-    print '</g>'
+#            print >>f,  svg.path( svg.polygon_path(slot), cut)
+        print >>f,  '</g>'
+    print >>f,  '</g>'
 
     for i, face in enumerate(faces):
 
-        x, y = i%10, i/10
+        x, y = 9-i%10, i/10
 
         eye = face['pos']
         north = face['points'][0]
@@ -69,10 +75,24 @@ if __name__ == '__main__':
         front = projection.inverse_project(m, eye=eye, north=north, front=True)
         front = [ map(lambda (x,y,vis): (x*radius,-y*radius, vis), p) for p in front ]
 
-        print '<g transform="translate('+str(w*x+w*.5)+' '+str(h*y+h*.5)+')">'
-        print regions_path(front, engrave, xtra=' clip-path="url(#face)"')
-#        print svg.path( facepath, clip )
-    	print '</g>'
+        print >>f,  '<g transform="translate('+str(w*x+w*.5)+' '+str(h*y+h*.4)+')" id="engrave_'+str(i)+'">'
+        print >>f,  svg.path( facepath, engrave )
+        print >>f,  regions_path(front, engrave )
+        print >>f,  '</g>'
 
-    print svg.footer()
+    print >>f,  svg.footer()
+    f.close()
+
+    argv = [ 'inkscape', filename ]
+
+    for i,_ in enumerate(faces):
+        if i in inverted:
+            action = 'SelectionDiff'
+        else:
+            action = 'SelectionIntersect'
+
+        argv += [ '--select=engrave_'+str(i), '--verb', 'SelectionUnGroup', '--verb', action, '--verb', 'EditDeselect' ]
+
+    argv += [ '--verb', 'FileSave', '--verb', 'FileQuit' ]
+    os.spawnvp(os.P_WAIT, 'inkscape', argv)
 
